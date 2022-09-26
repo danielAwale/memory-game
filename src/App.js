31c4d4130f0295ac28dee025651dff4f13dd57ce
danielAwale <danielmadhukarawale@gmail.com>
2022-09-26 10:01:50 -0700

    contain cardimages array and shufflecard function; have our first state as well

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/annoyed.png" },
+  { "src": "/img/bored.png" },
+  { "src": "/img/computertime.png" },
+  { "src": "/img/cuteface.png" },
+  { "src": "/img/pawwws.png" },
+  { "src": "/img/pets.png" },
+  { "src": "/img/shelftime.png" },
+  { "src": "/img/sleepy.png" },
+]
+
 function App() {
+  const [cards, setCards] = useState([]);
+
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+  }
+
+  console.log(cards);
   return (
     <div>
       <h1>Panda's Pic! Memory Game</h1>
-      <button>Let's Play!
+      <button onClick={shuffleCards}>Let's Play!
       </button>
     </div>
   );
