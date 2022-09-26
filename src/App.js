ec4b5bc2922d11ec9276cd5b2f9735412720b6fa
danielAwale <danielmadhukarawale@gmail.com>
2022-09-26 09:38:36 -0700

    contain basic styling for the page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello World!
+      <h1>Panda's Pic! Memory Game</h1>
+      <button>Let's Play!
+      </button>
     </div>
   );
 }
